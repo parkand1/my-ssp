8702ae596f1968e14fd830e9c7d3a347a26afed1
EC2 Default User <ec2-user@ip-172-31-27-182.us-west-2.compute.internal>
2022-03-15 05:49:52 +0000

    testing new users

--- teams/platform-team/index.ts
+++ teams/platform-team/index.ts
@@ -5,7 +5,9 @@ export class TeamPlatform extends PlatformTeam {
     constructor(accountID: string) {
         super({
             name: "platform", 
-            userRoleArn: `arn:aws:iam::059886156858:role/platform-team-role`
+            userRoleArn: `arn:aws:iam::059886156858:role/platform-team-role`,
+            users: [new ArnPrincipal('arn:aws:iam::059886156858:user/platform-user')]
+
         })
     }
 }
